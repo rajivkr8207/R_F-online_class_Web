44d9710566c504a8948780b8847f2d9f1c33c069
rajivkr8207 <silent.action786@gmail.com>
2024-03-15 21:13:23 +0530

    Online Courses

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,53 +7,53 @@ const Header = () => {
       url: "/",
       text: "home",
     },
-    // {
-    //   url: "/about",
-    //   text: "about",
-    // },
+    {
+      url: "/about",
+      text: "about",
+    },
     {
       url: "/course",
       text: "courses",
     },
-    // {
-    //   url: "/teacher",
-    //   text: "teacher",
-    // },
-    // {
-    //   url: "/blog",
-    //   text: "blog",
-    // }
-  ])
- 
+    {
+      url: "/teacher",
+      text: "teacher",
+    },
+    {
+      url: "/blog",
+      text: "blog",
+    },
+  ]);
+
   return (
     <>
-      <div className="flex justify-between px-14 py-3 ">
-        <div>Logo</div>
-
-        <div className="flex gap-7 ">
-          <div><ul className="flex gap-5">
-            {
-              Navurl.map((item, index) => {
+      <div className="flex lg:flex-row flex-col justify-between py-3 ">
+        {/* <div className="lg:ml-10">Logo</div> */}
+        <div></div>
+        <div className="flex  gap-7 lg:mx-0 lg:mt-0 mt-4 my-auto mx-auto">
+          <div>
+            <ul className="flex  gap-5 my-auto">
+              {Navurl.map((item, index) => {
                 return (
                   <li key={index}>
                     <NavLink
                       to={item.url}
                       activeClassName=" text-gray-900"
-                      className="px-3 py-2 rounded-md text-base font-medium capitalize"
+                      className="px-3 py-2 rounded-md text-base font-medium capitalize cursor-pointer"
                     >
                       {item.text}
                     </NavLink>
                   </li>
-                )
-              })
-            }
-            </ul></div>
+                );
+              })}
+            </ul>
+          </div>
 
           {/* <div>
             <NavLink
               to="/contact"
               activeClassName="bg-gray-200 text-gray-900"
-              className="px-3 py-2 text-sm font-medium capitalize border border-black"
+              className="px-3 py-2 text-sm font-medium capitalize border  border-black"
             >
               Contact
             </NavLink>
